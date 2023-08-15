8601829975765354557b589320d9266a9df531ab
Duvan <duvanserrano21@gmail.com>
2023-08-15 14:06:35 -0500

    Arreglado hover icon wh

--- src/components/icons/WhatsApp.jsx
+++ src/components/icons/WhatsApp.jsx
@@ -6,7 +6,10 @@ import style from './WhatsApp.module.css'
 function Whatsapp() {
     return (
         <div className={style.whatsappIconContainer}>
-            <div className={style.contactDiv}>Contáctanos</div>
+            <div className={style.contactDiv}>
+                Contáctanos
+            </div>
+            <div className={style.pestaña}></div>
             <a href="https://api.whatsapp.com/send?phone=TUNUMERODETÉLEFONO" className={style.whatsappIcon}>
                 <FontAwesomeIcon icon={faWhatsapp} />
             </a>

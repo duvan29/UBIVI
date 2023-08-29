8b8a6a2f2c08897a9461b500cd339b9baf0a1ce3
Duvan <duvanserrano21@gmail.com>
2023-08-28 20:58:18 -0500

    Adecuar footer a app

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,6 +13,9 @@ const Footer = () => {
         <a href="enlace-a-red-social-2" target="_blank" rel="noopener noreferrer">
           <img src="/icono-red-social-2.png" alt="Red social 2" />
         </a>
+        <a href="enlace-a-red-social-2" target="_blank" rel="noopener noreferrer">
+          <img src="/icono-red-social-2.png" alt="Red social 2" />
+        </a>
       </div>
     </footer>
   );

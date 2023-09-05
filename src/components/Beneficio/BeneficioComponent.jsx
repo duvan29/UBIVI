91d001418541875cadb072e8f88752de60fa8bc7
Duvan <duvanserrano21@gmail.com>
2023-09-05 09:44:40 -0500

    Beneficios acabados

--- src/components/Beneficio/BeneficioComponent.jsx
+++ src/components/Beneficio/BeneficioComponent.jsx
@@ -5,22 +5,22 @@ import styles from './BeneficiosComponent.module.css';
 const BeneficiosContainer = () => {
   const beneficiosData = [
     {
-      logo: 'lorebern',
+      logo: (<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M27.5 162.2L9 187.1h90.5l6.9-130.7-78.9 105.8zM396.3 45.7L267.7 32l135.7 147.2-7.1-133.5zM112.2 218.3l-11.2-22H9.9L234.8 458zm2-31.2h284l-81.5-88.5L256.3 33zm297.3 9.1L277.6 458l224.8-261.7h-90.9zM415.4 69L406 56.4l.9 17.3 6.1 113.4h90.3zM113.5 93.5l-4.6 85.6L244.7 32 116.1 45.7zm287.7 102.7h-290l42.4 82.9L256.3 480l144.9-283.8z"/></svg>),
       title: 'Orden',
       description: 'Generar un espacio ordenado cuyo cliente se sienta cómodo con el ambiente.',
     },
     {
-      logo: 'logo2.png',
+      logo: (<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M228.3 469.1L47.6 300.4c-4.2-3.9-8.2-8.1-11.9-12.4h87c22.6 0 43-13.6 51.7-34.5l10.5-25.2 49.3 109.5c3.8 8.5 12.1 14 21.4 14.1s17.8-5 22-13.3L320 253.7l1.7 3.4c9.5 19 28.9 31 50.1 31H476.3c-3.7 4.3-7.7 8.5-11.9 12.4L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9zM503.7 240h-132c-3 0-5.8-1.7-7.2-4.4l-23.2-46.3c-4.1-8.1-12.4-13.3-21.5-13.3s-17.4 5.1-21.5 13.3l-41.4 82.8L205.9 158.2c-3.9-8.7-12.7-14.3-22.2-14.1s-18.1 5.9-21.8 14.8l-31.8 76.3c-1.2 3-4.2 4.9-7.4 4.9H16c-2.6 0-5 .4-7.3 1.1C3 225.2 0 208.2 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141C165 36.5 211.4 51.4 244 84l12 12 12-12c32.6-32.6 79-47.5 124.6-39.9C461.5 55.6 512 115.2 512 185.1v5.8c0 16.9-2.8 33.5-8.3 49.1z"/></svg>),
       title: 'Salud',
       description: 'Buscar la mejora de la conducta humana a través del orden.',
     },
     {
-      logo: 'logo3.png',
+      logo: (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M272 96c-78.6 0-145.1 51.5-167.7 122.5c33.6-17 71.5-26.5 111.7-26.5h88c8.8 0 16 7.2 16 16s-7.2 16-16 16H288 216s0 0 0 0c-16.6 0-32.7 1.9-48.2 5.4c-25.9 5.9-50 16.4-71.4 30.7c0 0 0 0 0 0C38.3 298.8 0 364.9 0 440v16c0 13.3 10.7 24 24 24s24-10.7 24-24V440c0-48.7 20.7-92.5 53.8-123.2C121.6 392.3 190.3 448 272 448l1 0c132.1-.7 239-130.9 239-291.4c0-42.6-7.5-83.1-21.1-119.6c-2.6-6.9-12.7-6.6-16.2-.1C455.9 72.1 418.7 96 376 96L272 96z"/></svg>),
       title: 'Ambiente',
       description: 'Un ambiente tranquilo, ordenado con el cual el cliente se sienta una unión con él y el hogar.',
     },
     {
-      logo: 'logo4.png',
+      logo: (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M163.9 136.9c-29.4-29.8-29.4-78.2 0-108s77-29.8 106.4 0l17.7 18 17.7-18c29.4-29.8 77-29.8 106.4 0s29.4 78.2 0 108L310.5 240.1c-6.2 6.3-14.3 9.4-22.5 9.4s-16.3-3.1-22.5-9.4L163.9 136.9zM568.2 336.3c13.1 17.8 9.3 42.8-8.5 55.9L433.1 485.5c-23.4 17.2-51.6 26.5-80.7 26.5H192 32c-17.7 0-32-14.3-32-32V416c0-17.7 14.3-32 32-32H68.8l44.9-36c22.7-18.2 50.9-28 80-28H272h16 64c17.7 0 32 14.3 32 32s-14.3 32-32 32H288 272c-8.8 0-16 7.2-16 16s7.2 16 16 16H392.6l119.7-88.2c17.8-13.1 42.8-9.3 55.9 8.5zM193.6 384l0 0-.9 0c.3 0 .6 0 .9 0z"/></svg>),
       title: 'Bienestar',
       description: 'Forma distintiva de ser consciente de pensar, sentir y actuar.',
     },
@@ -32,7 +32,7 @@ const BeneficiosContainer = () => {
             {beneficiosData.map((beneficio, index) => (
               <BeneficioComponent
                 key={index}
-                imageSrc={beneficio.imageSrc}
+                logo={beneficio.logo}
                 title={beneficio.title}
                 description={beneficio.description}
               />
--- src/components/Beneficios/BeneficiosComponent.jsx
+++ src/components/Beneficios/BeneficiosComponent.jsx
@@ -3,9 +3,11 @@ import styles from './BeneficiosComponent.module.css';
 
 const BeneficioComponent = ({ logo, title, description }) => {
   return (
-    <div className={styles.beneficio}>
+    <div className={styles.mainDiv}>
       <div className={styles.iconText}>
-        {logo}
+       <div className={styles.icon}> 
+          {logo}
+       </div>
       </div>
       <h2 className={styles.title}>{title}</h2>
       <p className={styles.description}>{description}</p>

388d76f95f0888154bbd8fe5818eb2235b41f1b0
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-11-01 11:00:35 -0500

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Whatsapp from './components/icons/WhatsApp';
 import Products from './components/products/products'
 import NavBar from './components/Nav/NavBar';
 import MainCarousel from './components/Carucel/Carucel';
-// import Video from './components/video/video';
+import Video from './components/video/video';
 
 function App() {
 
@@ -19,7 +19,7 @@ function App() {
       <Products/>
       <CardList />
       <FoundersContainer/>
-      {/* <Video/> */}
+      <Video/>
       <div>
         <Whatsapp/>
       </div>

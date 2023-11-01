5db97d6aa9271e8dda5007463d36dcf1664ca928
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-11-01 11:10:56 -0500

    CAmbios

--- src/components/video/video.jsx
+++ src/components/video/video.jsx
@@ -6,7 +6,7 @@ const Video = () => {
     <div dangerouslySetInnerHTML={{
       __html: `
         <script src="//my.visme.co/visme-embed.js"></script>
-        <iframe id="embedded-project-iframe" title="ubivi" src="//my.visme.co/_embed/pvjqdm6g-ubivi" style="border: 0; width:100%; height:100%;" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen=""></iframe>
+        <iframe id="embedded-project-iframe" title="ubivi" src="//my.visme.co/_embed/pvjqdm6g-ubivi" style="border: 0; width:100%; height:100%;" webkitallowfullscreen="true" mozallowfullscreen="" allowfullscreen=""></iframe>
       `,
     }} />
 </div>
